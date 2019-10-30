d11ecb0a6d66cecaaa1f0a646bd197f40ff86eb3
alfian <alfian853@gmail.com>
2019-10-30 18:00:01 +0700

    implement firebase-admin, fixing error handling

---
+++ onestop-service/src/main/java/com/gdn/onestop/service/impl/MessagingServiceImpl.java
@@ -0,0 +1,29 @@
+package com.gdn.onestop.service.impl;
+
+import com.gdn.onestop.service.MessagingService;
+import com.google.firebase.messaging.AndroidConfig;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.Message;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+@Service
+public class MessagingServiceImpl implements MessagingService {
+
+    @Override
+    public void pushMessageToFirebase(String topic, Map<String, String> data) {
+
+        AndroidConfig androidConfig = AndroidConfig.builder()
+                .setTtl(Duration.ofMinutes(2).toMillis()).setCollapseKey(topic)
+                .setPriority(AndroidConfig.Priority.HIGH).build();
+
+        Message message = Message.builder().setAndroidConfig(androidConfig).setTopic(topic)
+                .putAllData(data)
+                .build();
+            FirebaseMessaging.getInstance().sendAsync(message);
+
+    }
+}
